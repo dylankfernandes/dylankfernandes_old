ccdaf399f118ad85d78c5f70e72a7b0cb48b3fa4
Dylan Fernandes <dylan.fernandes00@gmail.com>
2018-08-04 14:34:35 -0400

    deleting branch

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from '@storybook/react'
-
-function loadStories() {
-  require('../src/components/Button/Button.story')
-  require('../src/components/Navbar/Navbar.story')
-}
-
-configure(loadStories, module)

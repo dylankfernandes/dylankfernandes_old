cac7193e5055190400a51a4c79acd52bb228eac6
Dylan Fernandes <dylan.fernandes00@gmail.com>
2018-08-17 18:35:29 -0400

    finished single page website

--- src/pageLayouts/Home/Home.js
+++ src/pageLayouts/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from '../../components/Button/';
+import Navbar from '../../components/Navbar';
 
 const Home = (props) => (
   <div className="home">
@@ -7,8 +8,9 @@ const Home = (props) => (
       <div className="home__content">
         <h1 className="home__title">Hi, I'm Dylan Fernandes</h1>
         <p className="home__tagline">programmer, blogger, self development geek, student</p>
-        <Button>Resume</Button>
-        <Button>About Me</Button>
+        <Button link = "https://www.linkedin.com/in/dylan-fernandes-287744137/">Resume</Button>
+        <Button link = "https://github.com/dylankfernandes">Projects</Button>
+        <Navbar />
       </div>
     </div>
   </div>
